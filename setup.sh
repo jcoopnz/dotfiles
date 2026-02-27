254d83ff2a935dbd0aa1973f3d4acffe710f3acc
Jordan Cooper <jordancooper1195@gmail.com>
2026-02-27 16:22:23 +1300

    Installs Homebrew if not already installed

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install Homebrew if not present
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Install Homebrew packages
 brew bundle --file=$(pwd)/Brewfile
 
