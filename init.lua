f2bb64034e58a73c05e68c8a4be70379326ba7df
Jordan Cooper <jordancooper1195@gmail.com>
2026-09-10 08:16:46 +1200

    Enforce a specific initial lazy version based on release version

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyversion = "v11.17.5"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=" .. lazyversion, lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
