517aedeedafdc8faa9077ef75139f9fb329d6013
Jordan Cooper <jordancooper1195@gmail.com>
2026-09-18 08:21:08 +1200

    Removes this linter

--- init.lua
+++ init.lua
@@ -294,23 +294,6 @@ require("lazy").setup({
     },
 
     {
-      "mfussenegger/nvim-lint",
-      event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-      config = function()
-        local lint = require("lint")
-        lint.linters_by_ft = {
-          javascript = { "eslint" },
-          typescript = { "eslint" },
-        }
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-          callback = function()
-            require("lint").try_lint()
-          end,
-        })
-      end,
-    },
-
-    {
       "akinsho/bufferline.nvim",
       version = "*",
       event = "VeryLazy",
